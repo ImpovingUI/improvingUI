e00713f5a40f4ef84b4e84a914b9580ca4eb43d3
DanielFlores1223 <dany-232000@hotmail.com>
2022-06-13 12:20:43 -0500

    props in component alerts

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const Alert = () => {
+export interface AlertProps  {
+     show: boolean,
+     type: 'success' | 'error' | 'warning' | 'info',
+     filled: boolean,
+     closeAutomatic: boolean,
+     timeOut?: number,
+     message: string,
+     tittle?: string,
+     fullWidth?: boolean,
+     position: 'top' | 'bottom' | 'left' | 'right',
+     icon?: FC | string
+}
+
+
+export const Alert: FC<AlertProps> = ( {
+     show = true,
+     type= 'success',
+     filled=true,
+     closeAutomatic= true,
+     timeOut= 3000,
+     message= '',
+     tittle= '',
+     fullWidth= false,
+     position='right'
+ } ) => {
+
   return (
     <div>Alert</div>
   )
-}
 
-export default Alert
+}
