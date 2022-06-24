958ed78718f85a1cd11bc5bb49f3428ec11573b5
AlexeiNay <alexeisanchez66@gmail.com>
2022-06-24 15:36:36 -0500

    Adding the props to the modal

---
+++ src/components/Modal/Modal.interface.ts
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+export enum SizeEnum {
+    'sm' = 'sm',
+    'md' = 'md',
+    'lg' = 'lg',
+    'xl' = 'xl',
+}
+
+export interface ModalProps {
+    size: `${SizeEnum}`;
+    header?: string;
+    show: boolean;
+    children: JSX.Element;
+}
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, {FC, useState, useEffect} from 'react';
+import CloseIcon from '../Alert/icons/CloseIcon'
+import {ModalProps} from './Modal.interface';
+import './Modal.css'
+export const Modal: FC<ModalProps> = (
+  {
+    children,
+    show = false,
+    size = 'md',
+    header='',
+  }
 
-export const Modal = () => {
+) => {
+
+  const [open, setOpen] = useState(show);
+  const closeModal = () => {
+    setOpen(false )
+  }
+
+  useEffect(() => {
+    setOpen(show)
+  }, [show])
+
   return (
-    <div>Modal</div>
+    <div>
+      {
+        open && (
+          <div id="myModal" className="modal">
+            <div className="modal-content">
+              <div id='header'>
+              <h2>{header}</h2>
+                 {/* <span className="close">&times;</span> */}
+                  <div onClick={closeModal}>
+                    <CloseIcon />
+                  </div>
+              </div>
+
+              {
+                children
+              }
+             
+                <div>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto omnis, a nesciunt iste voluptas tenetur perferendis illo impedit repudiandae neque magnam itaque fugit dolores labore eaque animi ipsam ad dicta.
+                </div>
+                <div>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto omnis, a nesciunt iste voluptas tenetur perferendis illo impedit repudiandae neque magnam itaque fugit dolores labore eaque animi ipsam ad dicta.
+                </div>
+                <div>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto omnis, a nesciunt iste voluptas tenetur perferendis illo impedit repudiandae neque magnam itaque fugit dolores labore eaque animi ipsam ad dicta.
+                </div>
+                
+            </div>
+          </div>
+        )
+      }
+    </div>
+   
   )
 }
+
+// sass src/components/Modal/Modal.scss src/components/Modal/Modal.css --watch --no-source-map
