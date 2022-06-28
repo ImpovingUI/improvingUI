26116e64f6bacdaff32fc74ddcc7105fa998d990
DanielFlores1223 <dany-232000@hotmail.com>
2022-06-28 13:54:30 -0500

    Styles in modal

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { fireEvent, screen, userEvent } from '@storybook/testing-library';
 
 import {Modal} from './Modal';
+import { Button } from '../Button';
 
 export default {
     title:'ReactComponentLibrary/Modal',
@@ -17,7 +18,25 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args}/>;
 
 export const DefaultModal = Template.bind({});
     DefaultModal.args = {
-        
+        show: false,
+        header: 'Modal',
+        size: 'md',
+        children: (
+          <>
+           <div id='footer'>
+              <Button fullWidth={true}/>
+              <Button fullWidth={true}/>
+            </div>
+            <div id='body'>
+            <p>body</p>
+      
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, sint ad tempore doloribus commodi obcaecati aut optio earum dolores eos laboriosam fuga natus culpa incidunt odio perspiciatis. Recusandae, assumenda possimus?
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, sint ad tempore doloribus commodi obcaecati aut optio earum dolores eos laboriosam fuga natus culpa incidunt odio perspiciatis. Recusandae, assumenda possimus?
+              lorem*10
+            </div>
+           
+          </>
+        )
     }
 
 
